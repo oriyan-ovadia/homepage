3b19a4b7864f10ef349b67ed9894b71033cd4b18
Oriyan Ovadia <oriyan.ovadia@lemonade.com>
2023-09-27 16:56:07 +0300

    Fix faulty imports

--- components/pages/Home/Social/Social.tsx
+++ components/pages/Home/Social/Social.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Anchor } from '@/components/shared';
-import LinkedInIcon from '@/components/shared/icons/LinkedInIcon';
+import { Anchor } from 'components/shared';
+import LinkedInIcon from 'components/shared/icons/LinkedInIcon';
 import { linkedInIconCss, socialCss } from './Social.css';
 
 export function Social() {
